c3f97348fd5aa037a62e7b77a64eab4bfc59787e
Kyle Moore <20785882+YourFavoriteKyle@users.noreply.github.com>
2022-03-30 16:19:48 -0400

    Client Defined Styles
    Added client defined styles to tailwind.config.cjs
    Added font imports to base __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,13 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 <slot />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	mode: 'jit',
 
 	theme: {
-		extend: {}
+		screens: {
+			mobile: '375px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			desktop: '1440px',
+			'2xl': '1536px'
+		},
+		extend: {
+			colors: {
+				'white-cyan': '#ffffff',
+				'primary-green': '#fb7413',
+				'gray-blue': '#4e5d73',
+				'dark-gray-blue': '#323a49',
+				'dark-blue': '#1f2632'
+			},
+			fontFamily: { sans: ['Manrope', ...defaultTheme.fontFamily.sans] }
+		}
 	},
 
 	plugins: []
