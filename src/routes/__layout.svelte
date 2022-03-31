e87d12f3ef768c6afb184691cf0be67a342331c7
Kyle Moore <20785882+YourFavoriteKyle@users.noreply.github.com>
2022-03-31 15:31:07 -0400

    Update __layout.svelte
    Added CSS Font Loading

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,24 @@
 <script>
 	import '../app.css';
+	import { onMount } from 'svelte';
+
+	onMount(async function () {
+		let manrope = new FontFace(
+			'Manrope',
+			'url(https://fonts.googleapis.com/css2?family=Manrope:wght@500;600;700&display=swap)'
+		);
+
+		await document.fonts.add(manrope);
+	});
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<!-- <link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" />
 	<link
 		href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
 		rel="stylesheet"
-	/>
+	/> -->
 
 	<meta name="description" content="Frontend Mentor | Advice Generator App" />
 	<meta name="theme-color" content="#53FFAB" />
